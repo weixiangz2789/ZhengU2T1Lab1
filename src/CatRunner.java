b1ea4f55359fbf14b4680b515782262b16399e35
1E10 Laptop <sample@bths.edu>
2022-09-23 10:39:37 -0400

    Initial

--- src/CatRunner.java
+++ src/CatRunner.java
@@ -1,2 +1,7 @@
-package PACKAGE_NAME;public class CatRunner {
+public class CatRunner {
+    public static void main (String[] args) {
+        Cat cat2 = new Cat ("Steve", 9, 10);
+        cat2.printCatInfo();
+        cat2.introduce();
+    }
 }
--- src/RectangleRunner.java
+++ src/RectangleRunner.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class RectangleRunner {
+public class RectangleRunner {
+    public static void main(String[] args){
+
+        // creating one instance/object from the Rectangle class "blueprint"
+        Rectangle rect1 = new Rectangle (5,6);
+        rect1.printPerimeter(); // calling a method on the object
+
+        // creating ANOTHER instance/object from the Rectangle class
+        Rectangle rect2 = new Rectangle(7,10);
+        rect2.printPerimeter(); 
+
+        Rectangle rect3 = new Rectangle(9,9);
+        rect3.printPerimeter();
+
+    }
 }
